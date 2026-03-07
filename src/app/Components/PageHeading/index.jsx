9b5f130e3efef375b48e4db4b8f8a93ab091b1e2
amit04-07-1999 <ameykumar76@gmail.com>
2026-03-07 15:42:58 +0530

    dxgd

--- src/app/Components/PageHeading/index.jsx
+++ src/app/Components/PageHeading/index.jsx
@@ -1,36 +1,42 @@
 "use client"
-import Link from "next/link";
-import { useEffect, useState } from "react";
 
-const PageHeading = ({ data }) => {
-  const [urlSegments, setUrlSegments] = useState([]);
-  useEffect(() => {
-    const pathSegments = window.location.pathname
-      .split("/")
-      .filter((segment) => segment !== "");
-    setUrlSegments(pathSegments);
-  }, []);
-  return (
-    <div className="container">
-      <h1 className="cs_page_title">{data?.title}</h1>
-      <ol className="breadcrumb text-capitalize">
-        <li className="breadcrumb-item">
-          <Link href="/">Home</Link>
-        </li>
-        {urlSegments.map((segment, index) => (
-          <li key={index} className="breadcrumb-item">
-            {index < urlSegments.length - 1 ? (
-              <Link href={`/${urlSegments.slice(0, index + 1).join("/")}`}>
-                {segment}
-              </Link>
-            ) : (
-              <span className="active">{segment}</span>
-            )}
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
+
+const PageHeading = () => {
+  return null;
 };
 
 export default PageHeading;
+
+
+
+// const PageHeading = ({ data }) => {
+//   const [urlSegments, setUrlSegments] = useState([]);
+//   useEffect(() => {
+//     const pathSegments = window.location.pathname
+//       .split("/")
+//       .filter((segment) => segment !== "");
+//     setUrlSegments(pathSegments);
+//   }, []);
+//   return (
+//     <div className="container">
+//       <h1 className="cs_page_title">{data?.title}</h1>
+//       <ol className="breadcrumb text-capitalize">
+//         <li className="breadcrumb-item">
+//           <Link href="/">Home</Link>
+//         </li>
+//         {urlSegments.map((segment, index) => (
+//           <li key={index} className="breadcrumb-item">
+//             {index < urlSegments.length - 1 ? (
+//               <Link href={`/${urlSegments.slice(0, index + 1).join("/")}`}>
+//                 {segment}
+//               </Link>
+//             ) : (
+//               <span className="active">{segment}</span>
+//             )}
+//           </li>
+//         ))}
+//       </ol>
+//     </div>
+//   );
