5594c077e90b08f4d5c4ff43315f5f39277787ce
Amit Kumar Yadav <ameykumar76@gmail.com>
2026-06-15 15:56:57 +0530

    sss

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+// middleware.js
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
@@ -12,7 +13,3 @@ export function middleware(request) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: '/:path*',
-};
